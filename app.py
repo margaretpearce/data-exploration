f92b57cd48e672d0a17bea9214b2db0fc899a194
Margaret Pearce <margaret.pearce@gmail.com>
2016-11-01 21:11:45 -0400

    Added categorical variable summary, style cleanup

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
 from flask import Flask
 from flask import render_template
 from dataexp_flask import DataExplorer
+import paths
 import os
 
 app = Flask(__name__)
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads/')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-EXAMPLES_FOLDER = os.path.join(APP_ROOT, 'static/sampledata/')
-app.config['EXAMPLES_FOLDER'] = EXAMPLES_FOLDER
+# Path information
+app.config['UPLOAD_FOLDER'] = paths.UPLOAD_FOLDER
+app.config['EXAMPLES_FOLDER'] = paths.EXAMPLES_FOLDER
 
 @app.route('/')
 @app.route('/index')
 def index():
-    datastats = DataExplorer(os.path.join(app.config['EXAMPLES_FOLDER'], "titanic.csv"), "PassengerId", "Survived")
+    datastats = DataExplorer(os.path.join(app.config['EXAMPLES_FOLDER'], "titanic.csv"),
+                             "PassengerId",
+                             "Survived",
+                             "Titanic (Kaggle)")
 
-    data = {'name': "Titanic (Kaggle Training Set)",
-            'headers' : datastats.get_headers(),
-            'statslist' : datastats.get_stats()}
+    data = {'name': datastats.get_title(),
+            'headers' : datastats.get_headers_numeric(),
+            'headerscat': datastats.get_headers_nonnumeric(),
+            'statslist' : datastats.get_stats_numeric(),
+            'statslistcat' : datastats.get_stats_nonnumeric()
+            }
 
-    plot = {'url' : "static/sampledata/titanic.png"}
+    plot = {'url' : os.path.join(app.config['EXAMPLES_FOLDER'], "titanic.png")}
 
-    hist_src = datastats.get_histograms()
+    hist_src = datastats.get_histograms_numeric()
     hist = {'hist_urls' : hist_src}
 
     return render_template('index.html', data=data, plot=plot, hist=hist)
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/interactions')
+def interactions():
+    return render_template('interactions.html')
--- dataexp_flask.py
+++ dataexp_flask.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import seaborn as sns
 import os
+import paths
 
 class DataExplorer:
 
-    def __init__(self, datafile, idcolumn, labelcolumn):
+    def __init__(self, datafile, idcolumn, labelcolumn, title):
+        # CSV file, row id column name, label column name
         self.data = pd.read_csv(datafile)
         self.id_column = idcolumn
         self.label_column = labelcolumn
+        self.title = title
 
-        # Separate out the numeric fields (excluding ID)
+        # Numeric fields (excluding ID)
         self.numeric_data = self.data.select_dtypes(include=['int64', 'float64'])
         self.numeric_fieldnames = list(self.numeric_data.columns.values)
-        self.numeric_fieldnames.remove(self.id_column)
+        if self.id_column in self.numeric_fieldnames:
+            self.numeric_fieldnames.remove(self.id_column)
 
-        self.APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-        self.EXAMPLES_FOLDER = os.path.join(self.APP_ROOT, 'static/sampledata/')
+        # Non-numeric fields (excluding ID)
+        self.nonnumeric_data = self.data.select_dtypes(exclude=['int64', 'float64'])
+        self.nonnumeric_fieldnames = list(self.nonnumeric_data.columns.values)
+        if self.id_column in self.nonnumeric_fieldnames:
+            self.nonnumeric_fieldnames.remove(self.id_column)
 
-    def get_stats(self):
+    def get_title(self):
+        return self.title
+
+    def get_stats_numeric(self):
         # Get count, num missing, num unique, avg, median, max, min, standard dev, and variance for each numeric field
         data_count = list(map(lambda x: self.numeric_data[x].count(), self.numeric_fieldnames))
         data_missing = list(map(lambda x: self.numeric_data[x].isnull().sum(), self.numeric_fieldnames))
@@ -34,17 +44,35 @@ class DataExplorer:
                     data_min, data_std, data_var)
         return stats
 
-    def get_headers(self):
+    def get_stats_nonnumeric(self):
+        data_count = list(map(lambda x: self.nonnumeric_data[x].count(), self.nonnumeric_fieldnames))
+        data_missing = list(map(lambda x: self.nonnumeric_data[x].isnull().sum(), self.nonnumeric_fieldnames))
+        data_unique = list(map(lambda x: len(self.nonnumeric_data[x].unique()), self.nonnumeric_fieldnames))
+        data_mostcommon = list(map(lambda x: str("%s (%d)" % \
+                            (self.nonnumeric_data[x].value_counts().idxmax(),
+                             self.nonnumeric_data[x].value_counts().max())), self.nonnumeric_fieldnames))
+        data_leastcommon = list(map(lambda x: str("%s (%d)" % \
+                            (self.nonnumeric_data[x].value_counts().idxmin(),
+                             self.nonnumeric_data[x].value_counts().min())), self.nonnumeric_fieldnames))
+
+        stats = zip(self.nonnumeric_fieldnames, data_count, data_missing, data_unique, data_mostcommon,
+                    data_leastcommon)
+
+        return stats
+
+    def find_most_common(self, column_data):
+        # Get numeric categorical variables
+        factors = pd.factorize(column_data)
+        return factors.median()
+
+    def get_headers_numeric(self):
         # Return the header for the stats list
         return ["Feature", "Count", "Missing", "Unique", "Average", "Median", "Max", "Min", "StdDev", "Variance"]
 
-    def summary_plots(self):
-        # Remove rows with nulls and show summary graphs for all numeric features
-        features_only = self.numeric_fieldnames.copy()
-        features_only.remove(self.label_column)
-        return sns.pairplot(self.data.dropna(), hue=self.label_column, vars=features_only, diag_kind='kde')
+    def get_headers_nonnumeric(self):
+        return ["Feature", "Count", "Missing", "Unique", "Most Common (#)", "Least Common (#)"]
 
-    def get_histograms(self):
+    def get_histograms_numeric(self):
         hist = {}
         for feature in self.numeric_fieldnames:
             # Generate the histogram
@@ -52,7 +80,7 @@ class DataExplorer:
             hist_plot = sns.distplot(feature_data, bins=None, hist=True, kde=False, rug=False)
 
             # Save the histogram
-            full_url = os.path.join(self.EXAMPLES_FOLDER, str(feature + "_hist.png"))
+            full_url = os.path.join(paths.EXAMPLES_FOLDER, str(feature + "_hist.png"))
             fig = hist_plot.get_figure()
             fig.savefig(full_url)
 
@@ -60,5 +88,5 @@ class DataExplorer:
             sns.plt.clf()
 
             # Return the relative URL to the histogram
-            hist[feature] = "static/sampledata/" + str(feature + "_hist.png")
+            hist[feature] = paths.EXAMPLES_RELATIVE + str(feature + "_hist.png")
         return hist
