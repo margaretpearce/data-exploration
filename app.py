a0b4724b70a904e92f6fb0a65d0fd72448e29cc4
Margaret Pearce <margaret.pearce@gmail.com>
2016-11-22 21:48:07 -0500

    Getting file information from session

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import render_template, session
 from data_driver import DataDriver
 import paths
 import key
-import jsonpickle
 
 app = Flask(__name__)
 
@@ -13,10 +12,44 @@ app.config['EXAMPLES_FOLDER'] = paths.EXAMPLES_FOLDER
 app.secret_key = key.SECRET_KEY
 
 
+def selecteddataset():
+    data_file = None
+    data_title = None
+    data_id = None
+    data_label = None
+
+    # Check if the values are already in session
+    if "data_file" in session:
+        data_file = session["data_file"]
+    if "data_title" in session:
+        data_title = session["data_title"]
+    if "data_id" in session:
+        data_id = session["data_id"]
+    if "data_label" in session:
+        data_label = session["data_label"]
+
+    # Make sure that at least the file and title are populated, or else get it from the page
+    if data_file is None or data_title is None:
+        # Get the current selected values
+        data_file = "titanic.csv"
+        data_title = "Titanic"
+        data_id = "PassengerId"
+        data_label = "Survived"
+
+        # Save values in session for future requests
+        session["data_file"] = data_file
+        session["data_title"] = data_title
+        session["data_id"] = data_id
+        session["data_label"] = data_label
+
+    return data_file, data_title, data_id, data_label
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    driver = DataDriver("titanic.csv", "Titanic", "PassengerId", "Survived")
+    data_file, data_title, data_id, data_label = selecteddataset()
+    driver = DataDriver(data_file, data_title, data_id, data_label)
 
     # Get the JSON for the summary data
     summary_json = driver.load_summary_json()
@@ -24,13 +57,15 @@ def index():
     return render_template('index.html', data=summary_json)
 
 
-def datasetselected():
+def dataset_selection_changed():
+    # Get the dropdown values and save them in session
     return None
 
 
 @app.route('/univariate')
 def univariate():
-    driver = DataDriver("titanic.csv", "Titanic", "PassengerId", "Survived")
+    data_file, data_title, data_id, data_label = selecteddataset()
+    driver = DataDriver(data_file, data_title, data_id, data_label)
 
     # Get the JSON for the summary data
     features_json = driver.load_features_json()
@@ -41,7 +76,8 @@ def univariate():
 @app.route('/bivariate')
 def bivariate():
     # Read Titanic data
-    driver = DataDriver("titanic.csv", "Titanic", "PassengerId", "Survived")
+    data_file, data_title, data_id, data_label = selecteddataset()
+    driver = DataDriver(data_file, data_title, data_id, data_label)
 
     # Get the JSON for the summary data
     interactions_json = driver.load_interactions_json()
