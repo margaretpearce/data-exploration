82ead155edb77b861203fe2a5b2154c745e1e150
Margaret Pearce <margaret.pearce@gmail.com>
2016-11-22 22:43:35 -0500

    Ran on the iris data set

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template, session
+from flask import Flask, render_template, session, request
 from data_driver import DataDriver
 import paths
 import key
@@ -31,10 +30,10 @@ def selecteddataset():
     # Make sure that at least the file and title are populated, or else get it from the page
     if data_file is None or data_title is None:
         # Get the current selected values
-        data_file = "titanic.csv"
-        data_title = "Titanic"
-        data_id = "PassengerId"
-        data_label = "Survived"
+        data_file = "iris.csv"
+        data_title = "Iris"
+        data_id = "ID"
+        data_label = "Species"
 
         # Save values in session for future requests
         session["data_file"] = data_file
@@ -54,12 +53,33 @@ def index():
     # Get the JSON for the summary data
     summary_json = driver.load_summary_json()
 
-    return render_template('index.html', data=summary_json)
+    return render_template('index.html',
+                           data=summary_json,
+                           data_file=data_file,
+                           data_title = data_title,
+                           data_id = data_id,
+                           data_label = data_label)
 
-
+@app.route('/dataset_selection_changed', methods=['POST'])
 def dataset_selection_changed():
     # Get the dropdown values and save them in session
-    return None
+    new_selection = str(request.form["data_set_field"])
+    new_title = request.form["data_title_field"]
+    new_index = request.form["data_index_field"]
+    new_label = request.form["data_label_field"]
+
+    session["data_file"] = new_selection
+    session["data_id"] = new_index
+    session["data_label"] = new_label
+
+    rule = request.url_rule
+
+    if '/univariate' in rule.rule:
+        return univariate()
+    elif '/bivariate' in rule.rule:
+        return bivariate()
+    else:
+        return index()
 
 
 @app.route('/univariate')
@@ -70,7 +90,12 @@ def univariate():
     # Get the JSON for the summary data
     features_json = driver.load_features_json()
 
-    return render_template('univariate.html', mydata=features_json)
+    return render_template('univariate.html',
+                           mydata=features_json,
+                           data_file=data_file,
+                           data_title = data_title,
+                           data_id = data_id,
+                           data_label = data_label)
 
 
 @app.route('/bivariate')
@@ -82,4 +107,9 @@ def bivariate():
     # Get the JSON for the summary data
     interactions_json = driver.load_interactions_json()
 
-    return render_template('bivariate.html', data=interactions_json)
+    return render_template('bivariate.html',
+                           data=interactions_json,
+                           data_file=data_file,
+                           data_title = data_title,
+                           data_id = data_id,
+                           data_label = data_label)
