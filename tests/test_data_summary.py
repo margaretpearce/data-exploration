39ab4390d8a43c710188838fb5ae4a1d4e3901b0
Margaret Pearce <margaret.pearce@gmail.com>
2017-02-12 21:32:32 -0500

    moved tests into a class with setup methods

--- tests/test_data_summary.py
+++ tests/test_data_summary.py
@@ -1,76 +1,56 @@
 # Ugly hack to fix imports
-import sys, os
+import sys
+import os
 sys.path.insert(0, os.path.abspath('..'))
 
 from controllers.data_summary import DataSummary
 from model.summary import Summary
 
 
-def test_load_summary_json():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
+class TestDataSummary:
+    @classmethod
+    def setup_class(cls):
+        cls.dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
+        cls.summary = DataSummary(cls.dataset)
+        cls.summary.load_data()
 
-    # Confirm the data set can be loaded into a Summary object
-    summary_loaded = summary.load_summary_json()
-    actual = type(summary_loaded)
-    expected = Summary
+    def test_load_summary_json(self):
+        # Confirm the data set can be loaded into a Summary object
+        summary_loaded = self.summary.load_summary_json()
+        actual = type(summary_loaded)
+        expected = Summary
+        assert actual == expected
 
-    assert actual == expected
+    def test_get_num_records(self):
+        expected = 891
+        actual = self.summary.get_num_records()
+        assert expected == actual
 
-def test_get_num_records():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
-    summary.load_data()
+    def test_get_num_features(self):
+        expected = 12
+        actual = self.summary.get_num_features()
+        assert expected == actual
 
-    expected = 891
-    actual = summary.get_num_records()
+    def test_count_missing(self):
+        num_records = 891
+        missing = self.summary.count_missing(num_records)
+        max_missing = len(missing)
+        missing_sum = 0
 
-    assert expected == actual
+        for i in range(max_missing):
+            missing_sum += missing.get(i)
 
-def test_get_num_features():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
-    summary.load_data()
+        assert missing_sum == num_records
 
-    expected = 12
-    actual = summary.get_num_features()
+    def test_get_sample_returns_list(self):
+        sample = self.summary.get_sample(5, self.summary.get_features_list())
+        expected = list
+        actual = type(sample)
+        assert expected == actual
 
-    assert expected == actual
-
-def test_count_missing():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
-    summary.load_data()
-
-    num_records = 891
-    missing = summary.count_missing(num_records)
-    max_missing = len(missing)
-    missing_sum = 0
-
-    for i in range(max_missing):
-        missing_sum += missing.get(i)
-
-    assert missing_sum == num_records
-
-def test_get_sample_returns_list():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
-    summary.load_data()
-
-    sample = summary.get_sample(5, summary.get_features_list())
-    expected = list
-    actual = type(sample)
-
-    assert expected == actual
-
-def test_get_sample_size():
-    dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
-    summary = DataSummary(dataset)
-    summary.load_data()
-
-    sample_size = 5
-    sample = summary.get_sample(sample_size, summary.get_features_list())
-    expected = sample_size
-    actual = len(sample)
-
-    assert expected == actual
+    def test_get_sample_size(self):
+        sample_size = 5
+        sample = self.summary.get_sample(sample_size, self.summary.get_features_list())
+        expected = sample_size
+        actual = len(sample)
+        assert expected == actual
