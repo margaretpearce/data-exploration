125f5a39553fe6a7486ce185b674f8916a2ebb07
Margaret Pearce <margaret.pearce@gmail.com>
2017-02-14 22:18:29 -0500

    Unit tests for univariate

---
+++ tests/test_data_univariate.py
@@ -0,0 +1,46 @@
+# Ugly hack to fix imports
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+
+from controllers.data_univariate import DataUnivariate
+from model.features import Features
+
+
+class TestDataUnivariate:
+    @classmethod
+    def setup_class(cls):
+        cls.dataset = ['titanic.csv', 'Titanic', 'PassengerId', 'Survived', False]
+        cls.univariate = DataUnivariate(cls.dataset)
+        cls.univariate.load_data()
+
+    def test_load_univariate_json(self):
+        features_loaded = self.univariate.load_features_json()
+        actual = type(features_loaded)
+        expected = Features
+        assert actual == expected
+
+    def test_count_binary(self):
+        expected = 891
+        actual = self.univariate.get_count("Survived")
+        assert actual == expected
+
+    def test_count_string(self):
+        expected = 891
+        actual = self.univariate.get_count("Name")
+        assert actual == expected
+
+    def test_count_int(self):
+        expected = 891
+        actual = self.univariate.get_count("SibSp")
+        assert actual == expected
+
+    def test_count_float(self):
+        expected = 891
+        actual = self.univariate.get_count("Fare")
+        assert actual == expected
+
+    def test_count_missing_vals(self):
+        expected = 714
+        actual = self.univariate.get_count("Age")
+        assert actual == expected
